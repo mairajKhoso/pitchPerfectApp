1d125a783893a2c617f9075738d559bfe5910cbd
Mairaj khoso <mairajkhosok13@gmail.com>
2019-01-16 11:52:47 +0500

    first commit

---
+++ pitchPerfectApp/PitchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PitchViewController.swift
+//  pitchPerfectApp
+//
+//  Created by Apple Macbook on 15/01/2019.
+//  Copyright © 2019 Hivelet. All rights reserved.
+//
+
+import UIKit
+
+class PitchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- pitchPerfectApp/ViewController.swift
+++ pitchPerfectApp/ViewController.swift
@@ -7,14 +7,58 @@
 //
 
 import UIKit
+import AVFoundation
+class ViewController: UIViewController, AVAudioRecorderDelegate {
 
-class ViewController: UIViewController {
-
+    
+    var audioRecord = AVAudioRecorder()
+    @IBOutlet weak var RecordingLbl: UILabel!
+    @IBOutlet weak var recordBtn: UIButton!
+    @IBOutlet weak var stopBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        RecordingLbl.text = "Tap to record"
+        recordBtn.isEnabled = true
+        stopBtn.isEnabled = false
     }
 
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("view will appear called")
+    }
+    @IBAction func RecordBtnTapped(_ sender: UIButton) {
+        recordBtn.isEnabled = false
+        stopBtn.isEnabled = true
+        RecordingLbl.text = "Recording in progress"
+        
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as String
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath , recordingName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+        print(filePath)
+        
+        let session = AVAudioSession.sharedInstance()
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+        } catch {
+            print(error)
+        }
+        try! audioRecord = AVAudioRecorder(url: filePath!, settings: [:])
+        audioRecord.isMeteringEnabled = true
+        audioRecord.prepareToRecord()
+        audioRecord.record()
+    }
+    
+    @IBAction func StopBtnTapped(_ sender: UIButton) {
+        recordBtn.isEnabled = true
+        stopBtn.isEnabled = false
+        RecordingLbl.text = "Recording stopped"
+        // stop the recording
+        audioRecord.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
+    }
 }
 
